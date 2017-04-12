d73087da2358dfdf0e68b23034f2c0f4b0b4c178
shashikirang <shashikirang@ccs.neu.edu>
2017-04-12 16:16:27 -0400

    login page and profile page design fixed
    login page and profile page design fixed

--- public/client/views/home/controllers/home.controller.client.js
+++ public/client/views/home/controllers/home.controller.client.js
@@ -8,6 +8,7 @@
 
         //event handlers
         vm.login = login;
+        vm.register = register;
 
         function init() {
         }
@@ -34,6 +35,31 @@
                 }, function (err) {
                     vm.error = "User not found";
                 });
+        }
+
+        function register(user) {
+            if (user && user.username && user.password && user.password2) {
+                if (user.password === user.password2) {
+                    UserService
+                        .findUserByUsername(user.username)
+                        .then(function (user) {
+                            user = user.data;
+                            vm.error = "Username already taken";
+                        }, function(err) {
+                            UserService
+                                .register(user)
+                                .then(function (user) {
+                                    user = user.data;
+                                    $location.url("/host/" + user._id + "/event");
+                                },function (err) {
+                                    vm.error = "User Registration Failed";
+                                })
+                        })
+                }
+                else {
+                    vm.passworderror = "Password and Verify password must match"
+                }
+            }
         }
     }
 
